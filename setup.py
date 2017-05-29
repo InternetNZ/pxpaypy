5bf3aa8002f610df64f2d094400ad910dac1bbcc
Kesara Rathnayake <kesara@nzrs.net.nz>
2017-05-30 10:30:07 +1200

    Added Changelog and setup file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PxPayPy",
+    version="0.1.0-alpha",
+    packages=find_packages(),
+
+    install_requires=["defusedxml>=0.5", "requests>=2.13"],
+
+    author="Kesara Rathnayake",
+    author_email="kesara@nzrs.net.nz",
+    description="Python library for DPS PaymentExpress PxPay 2.0 API.",
+    long_description="""\
+Python library for DPS PaymentExpress PxPay 2.0 API. Also supports stored
+credit card payments via PaymentExpress PxPost.""",
+    license="GNU GPL v3",
+    keywords="dps paymentexpress pxpay pxpost api",
+    url="https://github.com/NZRS/pxpaypy",
+)
