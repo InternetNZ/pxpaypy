eb66137ba0893c040bf3ce132e382b32b44a410d
Kesara Rathnayake <kesara@nzrs.net.nz>
2018-08-14 11:23:15 +1200

    Release 0.1.2a1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PxPayPy",
-    version="0.1.1a2",
+    version="0.1.2a1",
     packages=find_packages(),
 
     install_requires=["defusedxml>=0.5", "requests>=2.13"],
